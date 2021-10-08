7796081b64073efad0a6b8f3a587d7d68d03fd73
JX Wang <wjianxu@gmail.com>
2021-10-09 06:36:09 +0800

    refactor: 重构json合并代码

--- gen-source.js
+++ gen-source.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const path = require('path');
 
 const srcDir = path.join(__dirname, 'src');
+const destJson = path.join(__dirname, 'docs', 'data.json');
 
-const src = fs
+const srcJsonList = fs
     .readdirSync(srcDir)
-    .filter((file) => file.endsWith('json'))
-    .map((file) => {
-        return JSON.parse(fs.readFileSync(path.join(srcDir, file)).toString());
+    .filter((filename) => filename.endsWith('json'))
+    .map((filename) => {
+        return JSON.parse(
+            fs.readFileSync(path.join(srcDir, filename)).toString()
+        );
     });
 
-fs.writeFileSync(
-    path.join(__dirname, 'docs', 'data.json'),
-    JSON.stringify(src, null, 4)
-);
+fs.writeFileSync(destJson, JSON.stringify(srcJsonList, null, 4));
 console.log('Over!');
